860ca5b593e5674b20c6eec3d45222dd6f5c4fc6
lazur2006 <kai-schiffer@web.de>
2022-07-13 15:33:11 +0200

    mdfd

--- lib/git_manager.py
+++ lib/git_manager.py
@@ -8,14 +8,16 @@ class git_manager():
 
     def __init__(self) -> None:
 
-        git_ssh_identity_file = os.path.expanduser('id_rsa')
-        #git_ssh_identity_file = os.path.expanduser('git')
-        git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
+        # Repo.clone_from('https://github.com/lazur2006/NNF.git', 'test', branch='main')
+        # git_ssh_identity_file = os.path.expanduser('id_rsa')
+        # #git_ssh_identity_file = os.path.expanduser('git')
+        # git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
 
-        with Git().custom_environment(GIT_SSH_COMMAND=git_ssh_cmd):
-            try:
-                Repo.clone_from('git@github.com:lazur2006/NNF_releases.git', 'test', branch='main')
-            except:
-                self.repository = git.Repo(os.getcwd() + '/test')
-                self.repository.remotes.origin.pull()
-        print('')
+
+        try:
+            print('START :: try to clone the git')
+            Repo.clone_from('https://github.com/lazur2006/NNF.git', 'test', branch='main')
+        except:
+            self.repository = git.Repo(os.getcwd() + '/test')
+            self.repository.remotes.origin.pull()
+        print('END')
